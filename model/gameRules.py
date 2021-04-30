2e3fcc465f9cb3c7058cbe713ef1157fd5be1eda
hydratedguy <andre.ryc1@gmail.com>
2021-04-30 16:56:51 -0300

    primeiro commit

---
+++ model/gameRules.py
@@ -0,0 +1,45 @@
+__all__ = ['dificuldade', 'geraSenha', 'checaResposta']
+
+from random import randint
+
+##cores:  vermelho = 1, verde = 2, azul = 3, amarelo = 4, rosa = 5, azul-ciano = 6, marrom = 7, roxo = 8
+
+def dificuldade(n):
+    global numPedras, numCores, limitJogadas, tentativa = []
+
+    if (n == 1):
+        numPedras = 4
+        numCores = 6
+        limitJogadas = 8
+    elif (n == 2):
+        numPedras = 5
+        numCores = 7
+        limitJogadas = 10
+    else:
+        numPedras = 6
+        numCores = 8
+        limitJogadas = 12
+
+
+def geraSenha():
+    global senha
+    senha = []
+    for _ in numPedras:
+        senha.append(random.randint(1, 8))
+
+
+def checaResposta()
+    global dicas
+
+    dicas = []
+    listaAux = []
+
+    for i in senha:
+        if senha[i] == tentativa[i]:
+            dicas.append(True)
+            listaAux.append(tentativa[i])
+        elif tentativa[i] in senha and tentativa[i] not in listaAux:
+            dicas.append(False)
+            listaAux.append(tentativa[i])
+
+
